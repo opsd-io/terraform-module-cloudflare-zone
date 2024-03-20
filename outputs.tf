d023419f7044e0bd1443e0fe937a52dbb7c70457
jpluta <jakub.pluta@zdt.io>
2024-03-20 14:28:44 +0100

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "name_servers" {
   description = "A list of Cloudflare-assigned name servers."
-  value = cloudflare_zone.main.name_servers
+  value       = cloudflare_zone.main.name_servers
 }
